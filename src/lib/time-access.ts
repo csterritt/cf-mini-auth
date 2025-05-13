45a924acc2619fe4345ab41336f470b22d1dcb41
csterritt <csterritt@gmail.com>
2025-05-13 10:52:04 -0400

    Rewritten time-access.ts to use a cookie for holding the delta. Tested.

--- src/lib/time-access.ts
+++ src/lib/time-access.ts
@@ -1,12 +1,12 @@
+import { deleteCookie, getCookie, setCookie } from 'hono/cookie'
+
 /**
  * Returns the current time as a Date object. Use this instead of calling new Date() directly.
  * Optionally, pass arguments to forward to Date constructor.
  * @module lib/time-access
  */
-let startTime: Date | null = null
-let startTimeSetAt: number = 0
 
-export const getCurrentTime = (...args: any[]): Date => {
+export const getCurrentTime = (c: any, ...args: any[]): Date => {
   // if (args.length === 0) { // PRODUCTION:UNCOMMENT
   //   return new Date() // PRODUCTION:UNCOMMENT
   // } // PRODUCTION:UNCOMMENT
@@ -14,36 +14,20 @@ export const getCurrentTime = (...args: any[]): Date => {
   // // @ts-ignore // PRODUCTION:UNCOMMENT
   // return new Date(...args) // PRODUCTION:UNCOMMENT
 
-  let delta: number = 0
-  if (startTime) {
-    delta = Date.now() - startTimeSetAt
-  }
-
+  const ds = getCookie(c, 'delta')
+  const delta = parseInt(ds == null || ds.toString().trim() === '' ? '0' : ds)
   if (args.length === 0) {
-    if (!startTime) {
-      return new Date()
-    }
-
-    return new Date(startTime.getTime() + delta)
-  }
-
-  // Forward all arguments to Date constructor
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  if (!startTime) {
-    // @ts-ignore
-    return new Date(...args)
+    return new Date(new Date().getTime() + delta)
   }
 
   // @ts-ignore
   return new Date(new Date(...args).getTime() + delta)
 }
 
-export const setCurrentTime = (time: Date) => {
-  startTime = time
-  startTimeSetAt = Date.now()
+export const setCurrentDelta = (c: any, delta: number) => {
+  setCookie(c, 'delta', String(delta), { path: '/' })
 }
 
-export const clearCurrentTime = () => {
-  startTime = null
-  startTimeSetAt = 0
+export const clearCurrentDelta = (c: any) => {
+  deleteCookie(c, 'delta', { path: '/' })
 }
--- tests/time-access.spec.ts
+++ tests/time-access.spec.ts
@@ -3,122 +3,141 @@
 // To run this, cd to this directory and type 'bun test'
 // ====================================
 
-import { describe, it } from 'node:test'
+import { beforeEach, describe, it } from 'node:test'
 import assert from 'node:assert'
 import { setTimeout } from 'timers/promises'
 import {
   getCurrentTime,
-  setCurrentTime,
-  clearCurrentTime,
+  setCurrentDelta,
+  clearCurrentDelta,
 } from '../src/lib/time-access'
 
 const approximatelyEqual = (v1: number, v2: number, epsilon = 0.001) =>
   Math.abs(v1 - v2) < epsilon
 
+const makeFakeContext = () => {
+  const storage = new Map()
+  const cookieStorage = {
+    get: (name: string) => {
+      // @ts-ignore
+      return storage.get(name)
+    },
+    set: (name: string, value: string) => {
+      // @ts-ignore
+      storage.set(name, value)
+    },
+  }
+
+  // c.req.raw.headers.get("Cookie");
+  // c.header("Set-Cookie", cookie, { append: true });
+  return {
+    req: {
+      raw: {
+        headers: {
+          get: (name: string) => {
+            if (name === 'Cookie' || name === 'cookie') {
+              return cookieStorage.get('Cookie')
+            }
+          },
+        },
+      },
+    },
+    header: (name: string, value: string, options?: any) => {
+      if (name === 'Set-Cookie') {
+        cookieStorage.set('Cookie', value)
+      } else {
+        throw new Error(`Unknown header ${name}`)
+      }
+    },
+  }
+}
+
 describe('getCurrentTime function', () => {
+  let c: any = null
+  beforeEach(() => {
+    c = makeFakeContext()
+  })
   it('should return the current no-args time when no time has been set', () => {
     assert(
-      approximatelyEqual(getCurrentTime().getTime(), new Date().getTime(), 5)
+      approximatelyEqual(getCurrentTime(c).getTime(), new Date().getTime(), 5)
     )
   })
 
   it('should return the correct no-args time when a time has been set in the past', () => {
-    setCurrentTime(new Date(new Date().getTime() - 50000))
+    setCurrentDelta(c, -50_000)
     assert(
       approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() - 50000,
+        getCurrentTime(c).getTime(),
+        new Date().getTime() - 50_000,
         5
       )
     )
   })
 
   it('should return the correct no-args time when a time has been set in the future', () => {
-    setCurrentTime(new Date(new Date().getTime() + 50000))
+    setCurrentDelta(c, 50_000)
     assert(
       approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() + 50000,
+        getCurrentTime(c).getTime(),
+        new Date().getTime() + 50_000,
         5
       )
     )
   })
 
   it('should return the correct no-args time with a delay when a time has been set in the past', async () => {
-    setCurrentTime(new Date(new Date().getTime() - 50000))
+    setCurrentDelta(c, -50_000)
     await setTimeout(100)
     assert(
       approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() - 50000,
+        getCurrentTime(c).getTime(),
+        new Date().getTime() - 50_000,
         105
       )
     )
   })
 
   it('should return the correct no-args time with a delay when a time has been set in the future', async () => {
-    setCurrentTime(new Date(new Date().getTime() + 50000))
+    setCurrentDelta(c, 50_000)
     await setTimeout(100)
     assert(
       approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() + 50000,
+        getCurrentTime(c).getTime(),
+        new Date().getTime() + 50_000,
         105
       )
     )
   })
 
-  it('should return the correct with-args time in the past', () => {
-    setCurrentTime(new Date(new Date().getTime() - 50000))
+  it('should return the correct with-args time based in the past', () => {
+    setCurrentDelta(c, -50_000)
+    const futureDate = new Date(new Date().getTime() + 100_000)
     assert(
       approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() - 50000,
+        getCurrentTime(c, futureDate).getTime(),
+        new Date().getTime() + 50_000,
         5
       )
     )
   })
 
-  it('should return the correct with-args time in the future', () => {
-    setCurrentTime(new Date(new Date().getTime() + 50000))
+  it('should return the correct with-args time based in the future', () => {
+    setCurrentDelta(c, 50_000)
+    const futureDate = new Date(new Date().getTime() + 100_000)
     assert(
       approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() + 50000,
+        getCurrentTime(c, futureDate).getTime(),
+        new Date().getTime() + 150_000,
         5
-      )
-    )
-  })
-
-  it('should return the correct with-args time with a delay in the past', async () => {
-    setCurrentTime(new Date(new Date().getTime() - 50000))
-    await setTimeout(100)
-    assert(
-      approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() - 50000,
-        105
-      )
-    )
-  })
-
-  it('should return the correct with-args time with a delay in the future', async () => {
-    setCurrentTime(new Date(new Date().getTime() + 50000))
-    await setTimeout(100)
-    assert(
-      approximatelyEqual(
-        getCurrentTime().getTime(),
-        new Date().getTime() + 50000,
-        105
       )
     )
   })
 
   it('should allow resetting the time properly', () => {
-    setCurrentTime(new Date(new Date().getTime() - 50000))
-    clearCurrentTime()
+    setCurrentDelta(c, -50_000)
+    clearCurrentDelta(c)
     assert(
-      approximatelyEqual(getCurrentTime().getTime(), new Date().getTime(), 5)
+      approximatelyEqual(getCurrentTime(c).getTime(), new Date().getTime(), 5)
     )
   })
 })
