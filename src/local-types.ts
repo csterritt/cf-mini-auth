c393a144510997f291b633e6a0038786f26086b2
csterritt <csterritt@gmail.com>
2025-05-16 19:16:10 -0400

    Added test to verify that on session time out, you're signed out.

--- src/local-types.ts
+++ src/local-types.ts
@@ -1,8 +1,18 @@
 import { Maybe } from 'true-myth'
 
+export type SignInSession = {
+  id: number
+  token: string
+  userId: number
+  signedIn: boolean
+  createdAt: Date
+  updatedAt: Date
+  expiresAt: Date
+}
+
 export type Bindings = {
   DB: D1Database
-  Session: Maybe<any>
+  Session: Maybe<SignInSession>
 }
 
 export class CountAndDecrement {
--- src/middleware/signed-in-access.ts
+++ src/middleware/signed-in-access.ts
@@ -1,10 +1,13 @@
-import { createMiddleware } from 'hono/factory'
 import { Context } from 'hono'
+import { deleteCookie } from 'hono/cookie'
+import { createMiddleware } from 'hono/factory'
 
-import { PATHS } from '../constants'
-import { redirectWithError } from '../lib/redirects'
-import type { Bindings } from '../local-types'
+import { COOKIES, PATHS } from '../constants'
+import { redirectWithError, redirectWithMessage } from '../lib/redirects'
+import { Bindings, SignInSession } from '../local-types'
 import { setupNoCacheHeaders } from '../lib/setup-no-cache-headers'
+import { deleteSession } from '../lib/db-access'
+import { getCurrentTime } from '../lib/time-access'
 
 /**
  * Middleware to restrict access to signed-in users only.
@@ -20,11 +23,23 @@ export const signedInAccess = createMiddleware<{ Bindings: Bindings }>(
       )
     }
 
-    const maybeSession = c.env.Session.value
+    const maybeSession: SignInSession = c.env.Session.value
     if (!maybeSession.signedIn) {
       return redirectWithError(
         c,
         PATHS.AUTH.SIGN_IN,
+        'You must sign in to visit that page'
+      )
+    }
+
+    // Check if session has expired
+    if (maybeSession.expiresAt < getCurrentTime(c)) {
+      await deleteSession(c.env.DB, maybeSession.id.toString())
+      deleteCookie(c, COOKIES.SESSION, { path: '/' })
+
+      return redirectWithMessage(
+        c,
+        PATHS.HOME,
         'You must sign in to visit that page'
       )
     }
