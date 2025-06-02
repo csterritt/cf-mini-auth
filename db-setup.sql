ff8b30140f6f6d8e70796ff3b7766dbd9b64be98
csterritt <csterritt@gmail.com>
2025-06-02 15:16:08 -0400

    Initial db-setup.sql.

---
+++ db-setup.sql
@@ -0,0 +1,2 @@
+insert into user (id, email, emailVerified, createdAt, updatedAt) values ('aaaaa', 'csterritt@gmail.com', true, '2025-04-30T04:53:26.997Z', '2025-04-30T04:53:26.997Z');
+insert into count (id, count) values ('foo', 0);
